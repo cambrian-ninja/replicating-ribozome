a6eae8a995172f70e68694f993d9542e2e2ec049
Brendan Coulson <brendan@headspacetech.com>
2022-05-29 12:44:18 +0200

    cleaning up warnings

--- src/test/java/ninja/cambrian/replicatingribozome/ST/StringTemplateTests.java
+++ src/test/java/ninja/cambrian/replicatingribozome/ST/StringTemplateTests.java
@@ -7,6 +7,7 @@ import org.stringtemplate.v4.STGroupFile;
 import org.testng.annotations.Test;
 
 import java.net.URL;
+import java.util.Objects;
 
 import static org.testng.Assert.assertEquals;
 
@@ -15,38 +16,40 @@ public class StringTemplateTests {
     @Test
     public void runHelloWorldTest() {
         ST hello = new ST("Hello, <name>");
-        hello.add("name","world");
-        assertEquals(hello.render(),"Hello, world");
+        hello.add("name", "world");
+        assertEquals(hello.render(), "Hello, world");
     }
 
     @Test
     public void runGroupDirTest() {
         URL directoryURL = this.getClass().getResource("test");
-        STGroup group = new STGroupDir(directoryURL);
+        STGroup group = new STGroupDir(Objects.requireNonNull(directoryURL));
         ST st = group.getInstanceOf("/decl");
-        st.add("type","int");
-        st.add("name","x");
-        st.add("value",0);
+        st.add("type", "int");
+        st.add("name", "x");
+        st.add("value", 0);
 
-        assertEquals(st.render(),"int x = 0;");
+        assertEquals(st.render(), "int x = 0;");
     }
 
     @Test
     public void runGroupFileTest() {
         URL fileURL = this.getClass().getResource("test/test.stg");
-        STGroup group = new STGroupFile(fileURL);
+        STGroup group = new STGroupFile(Objects.requireNonNull(fileURL));
         ST st = group.getInstanceOf("decl");
         st.add("type", "int");
         st.add("name", "x");
         st.add("value", 0);
-        assertEquals(st.render(),"int x = 0;");
+        assertEquals(st.render(), "int x = 0;");
     }
 
     @Test
     public void runModelTest() {
         ST st = new ST("<b>$u.id$</b>: $u.name$", '$', '$');
-        st.add("u", new User(999, "parrt"));
-        assertEquals(st.render(),"<b>999</b>: parrt");
+        User u = new User(999, "userName");
+        st.add("u", u);
+        assertEquals(u.getName(),"userName");
+        assertEquals(st.render(), "<b>999</b>: userName");
     }
 
     @Test
@@ -55,19 +58,27 @@ public class StringTemplateTests {
         st.addAggr("items.{ firstName ,lastName, id }", "Ter", "Parr", 99); // add() uses varargs
         st.addAggr("items.{firstName, lastName ,id}", "Tom", "Burns", 34);
 
-        assertEquals(st.render(),"99: Parr, Ter\n34: Burns, Tom\n");
+        assertEquals(st.render(), "99: Parr, Ter\n34: Burns, Tom\n");
 
     }
 
 
     public static class User {
         public int id; // template can directly access via u.id
-        private String name; // template can't access this
-        public User(int id, String name) { this.id = id; this.name = name; }
-        public boolean isManager() { return true; } // u.manager
-        public boolean hasParkingSpot() { return true; } // u.parkingSpot
-        public String getName() { return name; } // u.name
-        public String toString() { return id+":"+name; } // u
+        private final String name; // template can't access this
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        } // u.name
+
+        public String toString() {
+            return id + ":" + name;
+        } // u
     }
 
 }
